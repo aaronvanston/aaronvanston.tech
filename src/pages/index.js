1aa877f986c4e5989877475b22ff780cbc519a76
Aaron Vanston <aaronvanston@gmail.com>
2019-04-18 23:10:01 +1000

    chore: remove dead code

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,6 @@ import Container from '../components/Container/Container'
 import HomeSection from '../components/HomeSection/HomeSection'
 import PostSummary from '../components/PostSummary/PostSummary'
 
-
-// const IndexPage = () => (
-//   <Layout>
-//     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-//     <h1>Hi people</h1>
-//     <p>Welcome to your new Gatsby site.</p>
-//     <p>Now go build something great.</p>
-//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-//     </div>
-//     <Link to="/page-2/">Go to page 2</Link>
-//   </Layout>
-// )
-
 const Home = ({ data: { posts, projects } }) => (
   <Page>
     <Container>
